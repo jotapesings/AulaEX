b274c97ab8c5fa52b969a540352a0d1a1740b705
jotapesings <57123887+jotapesings@users.noreply.github.com>
2023-04-17 17:45:07 -0300

    Aula-17042023

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manager : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
+    [SerializeField] Transform _pos;
+    [SerializeField] Vector3 _motion;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _pos.localScale = new Vector3(10,2,2);
     }
 
     // Update is called once per frame
---
+++ Assets/Script/Person.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    [SerializeField] string _nome = "Jotape ";
+    [SerializeField] int _idade;
+    [SerializeField] float _velocidade;
+    [SerializeField] GameObject _personagem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
