0d51dfcbf93d4b4578ed45460abf266f0b278343
jotapesings <57123887+jotapesings@users.noreply.github.com>
2023-04-26 17:55:39 -0300

    condicionais

--- Assets/Script/Condicionais.cs
+++ Assets/Script/Condicionais.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class Condicionais : MonoBehaviour
 {
+    float _f1;
+    float _f2;
+
+    int _i1;
+    int _i2;
+
+    string _t1;
+    string _t2;
+
+    bool _b1;
+    bool _b2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (_i1 == _i2)
+        {
+            Debug.Log("_i1 é igual a _i2");
+
+        }
     }
+
+
 
     // Update is called once per frame
     void Update()
